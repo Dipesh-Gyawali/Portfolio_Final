2a561b3ffee195e531384b4517b2b4146ef81ede
Dipesh Gyawali <112164101+Dipesh-Gyawali@users.noreply.github.com>
2024-05-22 06:57:06 +0545

    feat: skills and about section added

--- src/Portfolio.js
+++ src/Portfolio.js
@@ -10,42 +10,42 @@ const Portfolio = () => {
                 <img src={require('./img/work1.jpg')} alt=""/>
 
                 <div class="portfolio__link">
-                    <a href="#" class="portfolio__link-name">View details</a>
+                    <a href="https://dipesh3.netlify.app/" target='_blank' class="portfolio__link-name">PROJECT NO:- 1</a>
                 </div>
             </div>
             <div class="portfolio__img">
                 <img src={require('./img/work2.jpg')} alt=""/>
 
                 <div class="portfolio__link">
-                    <a href="#" class="portfolio__link-name">View details</a>
+                    <a href="https://dipesh1.netlify.app/" target='_blank' class="portfolio__link-name">PROJECT NO:- 2</a>
                 </div>
             </div>
             <div class="portfolio__img">
                 <img src={require('./img/work3.jpg')} alt=""/>
 
                 <div class="portfolio__link">
-                    <a href="#" class="portfolio__link-name">View details</a>
+                    <a href="https://dipesh4.netlify.app/" target='_blank' class="portfolio__link-name">PROJECT NO:- 3</a>
                 </div>
             </div>
             <div class="portfolio__img">
                 <img src={require('./img/work4.jpg')} alt=""/>
 
                 <div class="portfolio__link">
-                    <a href="#" class="portfolio__link-name">View details</a>
+                    <a href="https://dipesh5.netlify.app/" target='_blank' class="portfolio__link-name">PROJECT NO:- 4</a>
                 </div>
             </div>
             <div class="portfolio__img">
                 <img src={require('./img/work5.jpg')} alt=""/>
 
                 <div class="portfolio__link">
-                    <a href="#" class="portfolio__link-name">View details</a>
+                    <a href="https://dipesh6.netlify.app/" target='_blank' class="portfolio__link-name">PROJECT NO:- 5</a>
                 </div>
             </div>
             <div class="portfolio__img">
                 <img src={require('./img/work6.jpg')} alt=""/>
 
                 <div class="portfolio__link">
-                    <a href="#" class="portfolio__link-name">View details</a>
+                    <a href="https://dipesh7.netlify.app/" target='_blank' class="portfolio__link-name">PROJECT NO:- 6</a>
                 </div>
             </div>
         </div>
